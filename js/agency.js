1c55b4ed0f14fc51e3cef3c0c888e0fd20ae6c0c
hervecarpentier <hervecarp@gmail.com>
2016-05-13 03:05:58 +0100

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -23,14 +23,6 @@ $(document).ready(function() {
 });
 
 
-function stopVideo(player) {
-   $('.videoFrame').each(function(index, element) { 
-       var src = element.src; 
-       element.src = ""; 
-       element.src = src;
-   })
-};
-
 $(document).ready(function() {
     var stopVideo = function(player) {
       var vidSrc = player.prop('src');
