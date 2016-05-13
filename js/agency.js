e16d54cd2bd13309299dd9f30b7537d70520e21d
hervecarpentier <hervecarp@gmail.com>
2016-05-13 03:37:26 +0100

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -20,6 +20,11 @@ function stopVideo(player) {
 
 $(document).ready(function() {
  $('.close-modal').click(stopVideo);
+ $('.portfolio-modal').on('hidden.bs.modal', function() {
+	    stopVideo($('#video'));
+	  });
+	});
+
 });
 
 
